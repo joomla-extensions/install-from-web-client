9c6ae9360ed21515df9731da722d46b9925ee389
zero-24 <zero-24@users.noreply.github.com>
2015-02-22 00:29:31 +0100

    Update webinstaller.script.php

--- plugins/installer/webinstaller/webinstaller.script.php
+++ plugins/installer/webinstaller/webinstaller.script.php
@@ -3,7 +3,7 @@
  * @package     Joomla.Plugin
  * @subpackage  Installer.webinstaller
  *
- * @copyright   Copyright (C) 2013 Open Source Matters, Inc. All rights reserved.
+ * @copyright   Copyright (C) 2013 - 2015 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE
  */
 
