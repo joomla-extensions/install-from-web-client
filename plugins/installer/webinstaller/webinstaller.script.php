b6653210524e7ae4a3a67e2440654fc68a9d1aa5
zero-24 <zero-24@users.noreply.github.com>
2015-02-22 21:10:13 +0100

    Update webinstaller.script.php

--- plugins/installer/webinstaller/webinstaller.script.php
+++ plugins/installer/webinstaller/webinstaller.script.php
@@ -3,7 +3,7 @@
  * @package     Joomla.Plugin
  * @subpackage  Installer.webinstaller
  *
- * @copyright   Copyright (C) 2013 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE
  */
 
@@ -12,15 +12,13 @@ defined('_JEXEC') or die;
 /**
  * Support for the "Install from Web" tab
  *
- * @package     Joomla.Plugin
- * @subpackage  System.webinstaller
- * @since       3.2
+ * @since  3.2
  */
 class plginstallerwebinstallerInstallerScript
 {
         public function postflight($route, $adapter)
         {
-            $db = JFactory::getDbo();
+            $db    = JFactory::getDbo();
             $query = 'UPDATE ' . $db->quoteName('#__extensions') . ' SET ' . $db->quoteName('enabled') . ' = 1 WHERE ' . $db->quoteName('type') . ' = ' . $db->quote('plugin') . ' AND ' . $db->quoteName('element') . ' = ' . $db->quote('webinstaller');
             $db->setQuery($query);
             $db->execute();
