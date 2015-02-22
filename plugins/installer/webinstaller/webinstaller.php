1b7118a27fd7a6dfd69d942a98eb5c3e8af8d99a
zero-24 <zero-24@users.noreply.github.com>
2015-02-22 21:09:41 +0100

    Update webinstaller.php

--- plugins/installer/webinstaller/webinstaller.php
+++ plugins/installer/webinstaller/webinstaller.php
@@ -3,7 +3,7 @@
  * @package     Joomla.Plugin
  * @subpackage  Installer.webinstaller
  *
- * @copyright   Copyright (C)  2013 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE
  */
 
@@ -20,9 +20,7 @@ if (version_compare(JVERSION, '3.0', '<'))
 /**
  * Support for the "Install from Web" tab
  *
- * @package     Joomla.Plugin
- * @subpackage  System.webinstaller
- * @since       3.2
+ * @since  3.2
  */
 class PlgInstallerWebinstaller extends JPlugin
 {
